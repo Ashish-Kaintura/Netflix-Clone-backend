5de728ff82a80b08ad3e7ca7ba70ba3581f98965
Ashish-Kaintura <110974134+Ashish-Kaintura@users.noreply.github.com>
2023-01-19 15:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require ("dotenv").config();
 const cors = require("cors");
 const User = require("./db/User");
 const app = express();
-const port = process.env.port || 5000
+const port = process.env.PORT|| 5000
 
 
 const Jwt =require('jsonwebtoken');
